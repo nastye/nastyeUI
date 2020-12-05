fdcdb4a25ec538b8fffeb661b05b5397d8d12861
Claus Ruepp <claus.ruepp@gmail.com>
2020-12-05 22:13:17 +0100

    accept debuffy fate

--- buffs.lua
+++ buffs.lua
@@ -2,8 +2,10 @@ local moveBuffs = function()
   BuffFrame:ClearAllPoints()
   BuffFrame:SetPoint("TOPRIGHT", Minimap, "TOPLEFT", -11, 0)
 
-  DebuffButton1:ClearAllPoints()
-  DebuffButton1:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMLEFT", -11, 0)
+  -- if DebuffButton1 then
+  --   DebuffButton1:ClearAllPoints()
+  --   DebuffButton1:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMLEFT", -11, 0)
+  -- end
 end
 
 hooksecurefunc("UIParent_UpdateTopFramePositions", moveBuffs)
