8e78ab38a94afdcb6ec1337e22041c8a0947ce96
Claus Ruepp <claus.ruepp@gmail.com>
2020-12-06 15:06:55 +0100

    savedvars initialization

--- SavedVariables.lua
+++ SavedVariables.lua
@@ -1,1 +1,3 @@
 if not nastyeUISaved then nastyeUISaved = {} end
+if not nastyeUISaved.lastAppliedChatVer then nastyeUISaved.lastAppliedChatVer = 0 end
+if not nastyeUISaved.sexyMapAskedOnce then nastyeUISaved.sexyMapAskedOnce = false end
