c00a532c6fe5ce3c7b857e32b14c29e97899bf5f
Claus Ruepp <claus.ruepp@gmail.com>
2021-12-07 12:11:56 +0100

    move tooltip anchor

--- Tooltips.lua
+++ Tooltips.lua
@@ -4,7 +4,7 @@
 
 function ToolTipHandler(self)
     self:SetOwner(UIParent,"ANCHOR_NONE")
-    self:SetPoint("BOTTOMRIGHT", UIParent, 0, 300)
+    self:SetPoint("BOTTOMRIGHT", UIParent, -280, 15)
 end
 
 hooksecurefunc("GameTooltip_SetDefaultAnchor", ToolTipHandler)
